e282bc8dc9c79b5614026ac3165917b1661f9c7e
ealizadeh <me@ealizadeh.com>
2021-01-19 23:53:04 -0500

    Output results in a table to HTML.

--- pypocket/pocket.py
+++ pypocket/pocket.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 
+import pandas as pd
 import requests
 
 from pypocket.utils import convert_epoch_to_datetime
@@ -27,10 +28,19 @@ class PocketArticle:
 
 
 class Pocket(object):
-    def __init__(self, consumer_key: str, access_token: str):
+    def __init__(
+        self, consumer_key: str, access_token: str, html_filename: str = "report"
+    ):
         self._consumer_key = consumer_key
         self._access_token = access_token
         self.pocket_endpoints = PocketAPI
+
+        # Handle both scenarios where the html_filename has either .html extension or not.
+        self.html_output_filename = (
+            html_filename
+            if html_filename.endswith(".html")
+            else f"{html_filename}.html"
+        )
 
     @staticmethod
     def _reformat_items(item):
@@ -64,3 +74,13 @@ class Pocket(object):
         )
         result_list = results.json()["list"]
         return [self._reformat_items(elem) for elem in result_list.values()]
+
+    def to_html(self):
+        results_df = pd.DataFrame(data=self.retrieve())
+        with open(f"./{self.html_output_filename}", "w", encoding="utf-8") as f:
+            f.write(
+                results_df.to_html(render_links=True, justify="center").replace(
+                    '<table border="1" class="dataframe">',
+                    '<table class="table table-striped">',
+                )  # use bootstrap styling
+            )
