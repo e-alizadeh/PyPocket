20547b292f41d1c72e1d8d134197784621c496d9
ealizadeh <me@ealizadeh.com>
2021-01-31 20:08:50 -0500

    Update README. Refactor Auth class

--- pypocket/auth.py
+++ pypocket/auth.py
@@ -1,4 +1,5 @@
 import webbrowser
+from typing import Union
 
 import requests
 
@@ -8,7 +9,7 @@ from pypocket.pocket_api_endpoints import PocketAPI
 class Auth:
     def __init__(self, consumer_key: str):
         self.consumer_key = consumer_key
-        self.request_token: str = ""
+        self.request_token: Union[str, None] = None
         self.endpoints = PocketAPI
 
     def get_request_token(self) -> str:
@@ -27,7 +28,13 @@ class Auth:
             self.request_token = req.content.decode().lstrip("code=")
             return self.request_token
 
-    def authorize_request_token_browser(self) -> None:
+    def authorize_request_token_browser(
+        self, request_token: Union[str, None] = None
+    ) -> None:
+        if request_token is None:
+            self.request_token = self.get_request_token()
+        else:
+            self.request_token = request_token
         auth_url = f"{self.endpoints.auth}?request_token={self.request_token}&redirect_uri=http://www.google.com"
         webbrowser.open(auth_url, new=2)
 
